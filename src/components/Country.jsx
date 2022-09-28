c5b0ca61c6c6013b975142a96e100e187520d634
Jacinto Parra <jacinto2014@gmail.com>
2022-09-28 16:45:07 -0300

    css modificar2

--- src/components/Country.jsx
+++ src/components/Country.jsx
@@ -1,12 +1,13 @@
 const Country = ({ country }) => {
 
-    return <li>
-
-        <p>Name: {country.name.official + " "}</p>
-        <p>Capital: {country.capital}</p>
-        <img src={country.flags.png + " "} alt="flag" />
-        <br />
-    </li>;
+    return <div className="feo">
+        <li>
+            <p>Name: {country.name.official + " "}</p>
+            <p>Capital: {country.capital}</p>
+            <img src={country.flags.png + " "} alt="flag" />
+            <br />
+        </li>
+    </div>;
 }
 
 export default Country;
